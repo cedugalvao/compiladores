d87ac3b46c24d13130b630621c98765fb10599f3
Castelo <90470038+Pedr0Castel0@users.noreply.github.com>
2023-10-05 21:15:18 -0300

    🔥 redefinindo close

--- python/api.py
+++ python/api.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify, make_response
-from tabela_de_token import main, close_browser  # Importe a função close_browser
+from tabela_de_token import main
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/analyze": {"origins": "http://localhost:3000"}})
-CORS(app, resources={r"/fechar": {"origins": "http://localhost:3000"}})
 
 
 @app.route('/analyze', methods=['POST'])
@@ -13,23 +12,12 @@ def analyze_code():
     code = data.get('code')
 
     # Chame a função main() do seu código Python com o código do cliente
-    result = main(code, "")
+    result = main(code)
 
     # Responder com a resposta da análise
     response_data = result
 
-    # Retorne a resposta JSON
-    response = make_response(jsonify(response_data), 200)
-
-    return response
-
-
-@app.route('/fechar', methods=['POST'])
-def close():
-    data = request.get_json()
-    fechar = data.get('fechar')
-    main("", fechar)
-    return True
+    return jsonify(response_data)
 
 
 if __name__ == '__main__':
